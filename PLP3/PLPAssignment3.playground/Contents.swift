85891b67397c6516eae2f4beb63268bbe02f4d23
Pam Qian <peizhuqian@gmail.com>
2019-02-13 13:10:38 -0500

    Add files via upload

---
+++ PLP3/PLPAssignment3.playground/Contents.swift
@@ -0,0 +1,24 @@
+
+var stringA = "Hello, this is stringA!"
+// stringA can be modified
+let stringB = "Hello, this is stringB"
+// stringB cannot be modified
+
+if stringA.isEmpty {
+    print( "stringA is empty!" )
+} else {
+    print( "stringA is not empty!" )
+}
+
+print(stringB)
+
+var varA:Float = 20.0 // float data type
+var constB = 100 // constant data type
+var varC:Int = 10 // integer data type
+
+print(varA)
+print(constB)
+print(varC)
+
+
+
